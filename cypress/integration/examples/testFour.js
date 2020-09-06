d942ea1c4de8c809763d04fa7aa98694aabec05e
CodyJReed <cody2j2reed@gmail.com>
2020-09-06 11:10:49 -0700

    Added testFour scenerio for iterating and capturing a target element's sibling

--- cypress/integration/examples/testFour.js
+++ cypress/integration/examples/testFour.js
@@ -3,5 +3,17 @@
 describe("My Fourth testSuite", () => {
   it("My fourth cypress test", () => {
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    cy.get("tr td:nth-child(2)").each(($e1, index, $list) => {
+      const text = $e1.text();
+      if (text.includes("Python")) {
+        cy.get("tr td:nth-child(2)")
+          .eq(index)
+          .next()
+          .then((price) => {
+            const priceText = price.text();
+            expect(priceText).to.equal("25");
+          });
+      }
+    });
   });
 });
